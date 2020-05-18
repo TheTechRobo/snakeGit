c2df3cd84a52bcbdbe43039647514323b43df5a0
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-18 15:39:09 -0400

    wip, does not work

--- pygit.py
+++ pygit.py
@@ -9,7 +9,7 @@ def commit(msg):
     """
     print("Commiting message...")
     p(["git", "commit -m pyGit Commit: %s" % msg], shell=False, stdout=sys.stdout, stderr=sys.stderr)
-    print("Attempted to commit. Look above to see if it was successful, it will show you info just as it would show you without pyGit.")
+    print("Attempted to commit message %s. Look above to see if it was successful, it will show you info just as it would show you without pyGit." % msg)
 def add(files):
     """
     Syntax: add("files")
@@ -19,6 +19,6 @@ def add(files):
     """
     print("Staging files...")
     p(["git", "add %s" % files], shell=False, stdout=sys.stdout, stderr=sys.stderr)
-    print("Attempted to stage files. Look above to see if it was successful, it will show you info just as it would show you without pyGit.")
+    print("Attempted to stage files %s. Look above to see if it was successful, it will show you info just as it would show you without pyGit." % files)
 if __name__ == "__main__":
     print("Interactivity is not yet supported.")
