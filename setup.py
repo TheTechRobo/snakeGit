5ae03ba33c59cde12f1da9afea50a2ea52126494
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-19 11:31:48 -0400

    pyGit Commit: Add it to Pip (\!\!)

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
 from setuptools import setup
 
 setup(
-    name='pyGit',
-    version='v.0.2.1-stable',
+    name='pyyGit',
+    version='0.2.1',
     description='the missing Python git module',
     license='DBAD',
-    packages=['pygit'],
+    packages=['pyGit'],
     author='ittussarom retals mail ynohtna',
     author_email='thetechrobo@outlook.com',
     keywords=['git', 'easy', 'thetechrobo'],
-    url='https://github.com/TheTechRobo/PyGit'
+    url='https://github.com/TheTechRobo/PyGit',
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+    project_urls={
+        'Documentation': 'https://github.com/thetechrobo/PyGit/wiki',
+        'Source': 'https://github.com/thetechrobo/pygit',
+        'Tracker': 'https://github.com/thetechrobo/pygit/issues',
+    },
+    python_requires='>=3',
 )
