c0b05f37613b801ca8c804427a2d69d41c7a183b
morassuttia3 <computer@Anfunfuns-MacBook-Air.local>
2020-05-19 10:54:22 -0400

    YESSSSSS A PIP PACKAGE :)

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyGit',
+    version='v.0.2.1-stable',
+    description='the missing Python git module',
+    license='DBAD',
+    packages=['pygit'],
+    author='ittussarom retals mail ynohtna',
+    author_email='thetechrobo@outlook.com',
+    keywords=['git', 'easy', 'thetechrobo'],
+    url='https://github.com/TheTechRobo/PyGit'
+)
