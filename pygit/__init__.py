2ae1e5c3b404ef4346989e4bb903bfa146085d19
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-08-04 14:32:35 -0400

    Update __init__.py

--- pygit/__init__.py
+++ pygit/__init__.py
@@ -8,14 +8,6 @@ def parseIni():
     config.read('config.ini')
     try:
         brandingYesNo = config['DEFAULT'].getboolean('branding')
-        """
-        if config['main']['branding'].lower() in ['yes', 'on', 'enabled', 'enable', 'true', '1']:
-            brandingYesNo = True
-        elif config['main']['branding'].lower() in ['no', 'off', 'disable', 'disabled', 'false', '0']:
-            brandingYesNo = False
-        else:
-            raise ValueError
-        """
     except ValueError:
         print("Syntax error in config.ini: branding configuration must be ON or OFF.")
         print("Proceeding with branding ENABLED.")
