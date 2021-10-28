085398ea6f1d538d47436d094c3b81bce5aab7c3
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-10-27 21:16:26 -0400

    Require 3.7

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
-
+import os
+with open(os.devnull, 'w') as a:
+    print("If this raises an error, you're using python 2 - not supported.", file=a) #get rid of python 2 users
 with open("README.md", "r") as file:
     long_desc = file.read()
-
+import sys
+if sys.version_info < (3,7):
+    sys.exit('Sorry, Python < 3.7 is not supported')
 setup(
     name='pyyGit',
     version='0.4',
     description='the missing Python git module',
     long_description=long_desc,
+    python_requires='>3.7.0',
     license='DBAD',
     packages=['pyGit'],
     author='TheTechRobo',
@@ -28,5 +33,4 @@ setup(
 
     long_description_content_type='text/markdown',
 
-    python_requires='>=3',
 )
